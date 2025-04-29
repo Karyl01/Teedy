968e9713f73c87581e156daa62da9e5cccb96d91
Karyl01 <148410288+Karyl01@users.noreply.github.com>
2025-04-29 15:23:28 +0800

    现在成功在顶部创建了一个审批/请求的按钮以及一个实时通信的按钮，同时建立vue3界面在docs-web下

---
+++ docs-web/approve_and_chat/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <RegisterRequest />
+  </div>
+</template>
+
+<script setup>
+import RegisterRequest from './views/RegisterRequest.vue'
+</script>
+
+<style scoped>
+/* 可自定义样式 */
+</style>
